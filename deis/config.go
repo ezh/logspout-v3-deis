a1ff21d783a98d2e99c7dd42199737890fad2299
Kent Rancourt <krancourt@engineyard.com>
2015-08-25 11:29:09 -0400

    Adding a comment to point out a known bug.

--- deis/config.go
+++ deis/config.go
@@ -41,6 +41,7 @@ func (j *deisConfigJob) Setup() error {
 func (j *deisConfigJob) Run() error {
 	for {
 		// TODO: Save current route instead of just the current log host
+		// TODO: There's also an error in here and we're adding a route every minute
 		var currentHost string
 		if j.etcd != nil {
 			hostResp, err := j.etcd.Get("/deis/logs/host", false, false)
